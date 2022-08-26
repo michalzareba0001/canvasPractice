a140a4ef435fa4e63ef05ff7fb1de9fc31d1de92
Michał Zaręba <michalzareba0001@gmail.com>
2022-08-27 00:13:59 +0200

    job done screen added

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ window.addEventListener("resize", function () {
 
 });
 
+
+
+
 const mouse = {
     x: 0,
     y: 0,
@@ -39,21 +42,23 @@ class FoldFieldEffect {
     #height;
     constructor(ctx, width, height) {
         this.#ctx = ctx;
-        this.#ctx.strokeStyle = "rgb(255,255,0)";
-        this.#ctx.lineWidth = 3;
+        this.#ctx.strokeStyle = "rgb(100,200,50)";
+        this.#ctx.lineWidth = 2;
         this.#width = width;
         this.#height = height;
-        this.x = 0;
+        this.x = 2 * this.#width / 3;
         this.y = 0;
-        this.angle = 0;
-        this.interval = 1000;
+        this.angle = 0.1;
+        this.lastTime = 0;
+        this.interval = 1000/60;
         this.timer = 0;
+        this.cellSize = 50;
     }
-    #draw(x, y) {
-        const lenght = 200;
+    #drawLine(x, y) {
+        const lenght = 20;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(mouse.x, mouse.y);
+        this.#ctx.lineTo(mouse.x * 0.9, mouse.y);
         this.#ctx.stroke();
     }
 
@@ -61,8 +66,24 @@ class FoldFieldEffect {
     animate(timeStamp) {
         const deltaTime = timeStamp - this.lastTime;
         this.lastTime = timeStamp;
-        //this.#ctx.clearRect(0, 0, this.#width, this.#height);
-        this.#draw(this.#width / 2, this.#height / 2);
+        if (this.timer > this.interval){
+        this.#ctx.clearRect(0, 0, this.#width, this.#height);
+
+        for (let y = 0; y < this.#height; y += this.cellSize) {
+            for (let x = 0; x < this.#width; x += this.cellSize) {
+                
+                this.#drawLine(x, y);
+            }
+           
+        }
+        
+
+        
+        this.timer = 0;
+        }
+        else {
+            this.timer += deltaTime;
+        }
     /*   this.x += 2 + Math.sin(this.angle) * +10;
        this.y += 3 + Math.cos(this.angle) * -1;
        if (this.y < this.#height) { 
